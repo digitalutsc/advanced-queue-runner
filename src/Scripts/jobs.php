0cd1c29f0df971f1afd80da880f13e9435681cad
root <root@digitalscholarship.utsc.utoronto.ca>
2021-06-23 17:24:19 -0400

    move drush path and root path out of event loop to avoid repeat

--- src/Scripts/jobs.php
+++ src/Scripts/jobs.php
@@ -12,11 +12,11 @@ function run_command(string $command): void
     $process->on('exit', function ($exitCode) use ($command) {
         // Trigger alerts that the command finished.
         //error_log(print_r("Exit....", true), 0);
-  });
+    });
     $process->stdout->on('data', function ($chunk) {
         // Optinally log the output.
         //error_log(print_r("Data....", true), 0);
-      \Drupal::logger('runner')->notice($chunk);
+        \Drupal::logger('runner')->notice($chunk);
     });
     $process->stdout->on('error', function (Exception $e) use ($command) {
         // Log an error.
@@ -58,17 +58,28 @@ $interval = $config->get('interval');
 $mode = $config->get('mode');
 $base_url = $config->get('base_url');
 
+$config = \Drupal::config('advancedqueue_runner.runnerconfig');
+$drush_path = $config->get('drush_path');
+$root_path = $config->get('root_path');
+
+//$output = null;
+//$retval = null;
+//shell_exec('echo $HOME', $output, $retval);
+//if ($retval == 0 && is_array($output) && empty($output[0])) {
+//shell_exec("export HOME=" . $root_path, $output, $retval);
+//shell_exec('echo $HOME', $output, $retval);
+//}
+//\Drupal::logger('runner2')->notice(json_encode($output));
+//\Drupal::logger('runner2')->notice($retval);
+
 $loop = React\EventLoop\Factory::create();
-$loop->addPeriodicTimer($interval, function () use ($queues, $mode, $base_url, $kernel) {
+$loop->addPeriodicTimer($interval, function () use ($queues, $mode, $base_url, $kernel, $drush_path, $root_path) {
     foreach ($queues as $queue) {
-      $config = \Drupal::config('advancedqueue_runner.runnerconfig');
-      $drush_path  = $config->get('drush_path');
-      $root_path  = $config->get('root_path');
 
-      $command = sprintf($drush_path .  ' --root='.$root_path.' --uri=' .  parse_url($base_url, PHP_URL_HOST) . ' advancedqueue:queue:process ' . $queue);
-      //$command = sprintf($drush_path. ' --root=/var/www/core-D9/ advancedqueue:queue:process ' . $queue);
+        $command = sprintf($drush_path . ' --root=' . $root_path . ' --uri=' . parse_url($base_url, PHP_URL_HOST) . ' advancedqueue:queue:process ' . $queue);
+        //$command = sprintf($drush_path. ' --root=/var/www/core-D9/ advancedqueue:queue:process ' . $queue);
 
-      if ($mode === 'limit') {
+        if ($mode === 'limit') {
             $connection = $kernel->getContainer()->get('database');
             $jobs = $connection->query("SELECT count(job_id) FROM advancedqueue where queue_id = '$queue' and state = 'queued'")->fetchCol()[0];
 
@@ -77,8 +88,7 @@ $loop->addPeriodicTimer($interval, function () use ($queues, $mode, $base_url, $
                 \Drupal::logger('runner')->notice(sprintf($command));
                 run_command(sprintf($command));
             }
-        }
-        else {
+        } else {
             run_command(sprintf($command));
         }
 
