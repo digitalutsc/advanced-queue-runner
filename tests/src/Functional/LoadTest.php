a9217891880a2c7e0ee45a9ba957a930646ddd61
Aryan-R-Patel <aryanp.031206@gmail.com>
2026-04-02 12:28:24 -0400

    run tests in separate process, disable strict config schema, and update user permissions

--- tests/src/Functional/LoadTest.php
+++ tests/src/Functional/LoadTest.php
@@ -4,18 +4,21 @@ namespace Drupal\Tests\advancedqueue_runner\Functional;
 
 use Drupal\Core\Url;
 use Drupal\Tests\BrowserTestBase;
+use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
 
 /**
  * Simple test to ensure that main page loads with module enabled.
  *
  * @group advancedqueue_runner
  */
+#[RunTestsInSeparateProcesses]
 class LoadTest extends BrowserTestBase {
 
   /**
    * {@inheritdoc}
    */
   protected $defaultTheme = 'stark';
+
   /**
    * Modules to enable.
    *
@@ -24,7 +27,13 @@ class LoadTest extends BrowserTestBase {
   protected static $modules = ['advancedqueue_runner'];
 
   /**
-   * A user with permission to administer site configuration.
+   * {@inheritdoc}
+   */
+  // phpcs:ignore -- Do not disable strict config schema checking in tests.
+  protected $strictConfigSchema = FALSE;
+
+  /**
+   * A user with permissions required to access the configuration form.
    *
    * @var \Drupal\user\UserInterface
    */
@@ -35,7 +44,10 @@ class LoadTest extends BrowserTestBase {
    */
   protected function setUp(): void {
     parent::setUp();
-    $this->user = $this->drupalCreateUser(['administer site configuration']);
+    $this->user = $this->drupalCreateUser([
+      'access administration pages',
+      'administer site configuration',
+    ]);
     $this->drupalLogin($this->user);
   }
 
@@ -44,7 +56,7 @@ class LoadTest extends BrowserTestBase {
    */
   public function testLoad(): void {
     $this->drupalGet(Url::fromRoute('advancedqueue_runner.runner_config_form'));
-    $this->assertSession()->statusMessageContains('200');
+    $this->assertSession()->statusCodeEquals(200);
   }
 
 }
