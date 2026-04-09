2d8234146774cb591443ace134154b41316c1d1b
Aryan-R-Patel <aryanp.031206@gmail.com>
2026-04-09 15:11:57 -0400

    refactor: remove phpcs ignore comments by correcting their logic

--- src/DefaultService.php
+++ src/DefaultService.php
@@ -2,17 +2,45 @@
 
 namespace Drupal\advancedqueue_runner;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+
 /**
  * Class DefaultService definition.
  */
 class DefaultService implements DefaultServiceInterface {
 
   /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a new DefaultService object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   The entity type manager.
+   */
+  public function __construct(EntityTypeManagerInterface $entityTypeManager) {
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
    * Implements countJob().
    */
   public function countJob(String $queue) {
-    // phpcs:ignore -- \Drupal calls should be avoided in classes, use dependency injection instead
-    $entity = \Drupal::entityTypeManager()->getStorage('advancedqueue_queue')->load($queue);
+    $entity = $this->entityTypeManager->getStorage('advancedqueue_queue')->load($queue);
     $jobs = $entity->getBackend()->countJobs()['queued'];
     return $jobs;
   }
